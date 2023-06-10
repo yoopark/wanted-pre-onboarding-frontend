904320e4faeca8bc6c50cc6ac66c504e10782094
Yongjun Park <yopark@student.42seoul.kr>
2023-06-11 07:05:01 +0900

    ✨ feat: Todo 수정 모드 뷰

--- src/features/TodoPage/TodoItem.tsx
+++ src/features/TodoPage/TodoItem.tsx
@@ -1,4 +1,5 @@
 import type { Todo } from '@/types/Todo';
+import { useState } from 'react';
 
 type TodoItemProps = {
   todo: Todo;
@@ -11,20 +12,68 @@ export const TodoItem = ({
   onCheckboxClick,
   onDeleteBtnClick,
 }: TodoItemProps) => {
+  const [isModifyMode, setIsModifyMode] = useState<boolean>(false);
+  const [modifyTodoInput, setModifyTodoInput] = useState<string>(todo.todo);
+
+  const handleModifyBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsModifyMode(true);
+  };
+
+  const handleSubmitBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    /* 수정 로직 */
+    setIsModifyMode(false);
+  };
+
+  const handleCancelBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setModifyTodoInput(todo.todo);
+    setIsModifyMode(false);
+  };
+
+  const handleChangeModifyTodoInput = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setModifyTodoInput(value);
+  };
+
   return (
     <li>
-      <label>
-        <input
-          type="checkbox"
-          checked={todo.isCompleted}
-          onChange={onCheckboxClick}
-        />
-        <span>{todo.todo}</span>
-      </label>
-      <button data-testid="modify-button">수정</button>
-      <button data-testid="delete-button" onClick={onDeleteBtnClick}>
-        삭제
-      </button>
+      {!isModifyMode ? (
+        <>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={onCheckboxClick}
+            />
+            <span>{todo.todo}</span>
+          </label>
+          <button data-testid="modify-button" onClick={handleModifyBtnClick}>
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={onDeleteBtnClick}>
+            삭제
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            data-testid="modify-input"
+            value={modifyTodoInput}
+            onChange={handleChangeModifyTodoInput}
+          />
+          <button data-testid="submit-button" onClick={handleSubmitBtnClick}>
+            제출
+          </button>
+          <button data-testid="cancel-button" onClick={handleCancelBtnClick}>
+            취소
+          </button>
+        </>
+      )}
     </li>
   );
 };
