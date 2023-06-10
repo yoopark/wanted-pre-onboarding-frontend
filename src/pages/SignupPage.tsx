6e884ef34af13cc0910173b4e9f92e2fd78bcca8
Yongjun Park <yopark@student.42seoul.kr>
2023-06-11 00:51:59 +0900

    ✨ feat: SignupFormData

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,10 +1,37 @@
+import { useForm } from '@/utils/useForm';
+
+interface SignupFormData {
+  email: string;
+  password: string;
+}
+
 const SignupPage = () => {
+  const { formData, handleChange } = useForm<SignupFormData>({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
       <h1>회원가입</h1>
-      <form>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
+      <form onSubmit={handleSubmit}>
+        <input
+          data-testid="email-input"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
         <button data-testid="signup-button">회원가입</button>
       </form>
     </>
