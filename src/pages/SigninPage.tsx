a1a552d8c7fed3b56e76f6051c8d539fe4f09d0d
Yongjun Park <yopark@student.42seoul.kr>
2023-06-10 18:44:25 +0900

    ✨ [Feature] 기본 마크업 (#6)
    * ✨ feat: markup SigninPage
    
    * ✨ feat: markup SignupPage
    
    * ✨ feat: markup TodoPage

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -5,6 +5,11 @@ const SigninPage = () => {
   return (
     <>
       <h1>로그인</h1>
+      <form>
+        <input data-testid="email-input" />
+        <input data-testid="password-input" />
+        <button data-testid="signin-button">로그인</button>
+      </form>
       <span>아직 계정이 없다면&nbsp;</span>
       <span>
         <Link to={ROUTES.SIGNUP}>회원가입</Link>
