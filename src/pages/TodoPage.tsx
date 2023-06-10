35051631d0be26d42fa33d4821e5e645e4c1a3a6
Yongjun Park <yopark@student.42seoul.kr>
2023-06-11 04:54:53 +0900

    ✨ feat: todos view using map

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,18 +1,25 @@
+import { Todo } from '@/types/Todo';
+import { useState } from 'react';
+
 const TodoPage = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   return (
     <>
       <h1>TODO</h1>
       <input data-testid="new-todo-input" />
       <button data-testid="new-todo-add-button">추가</button>
       <ul>
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>TODO 1</span>
-          </label>
-          <button data-testid="modify-button">수정</button>
-          <button data-testid="delete-button">삭제</button>
-        </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.isCompleted} />
+              <span>{todo.todo}</span>
+            </label>
+            <button data-testid="modify-button">수정</button>
+            <button data-testid="delete-button">삭제</button>
+          </li>
+        ))}
       </ul>
     </>
   );
