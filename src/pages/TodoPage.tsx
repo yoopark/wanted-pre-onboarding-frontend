7d439ed35a0dd288d4f3e63af8c0515d9825f1b5
Yongjun Park <yopark@student.42seoul.kr>
2023-06-10 18:42:03 +0900

    ✨ feat: markup TodoPage

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -2,6 +2,18 @@ const TodoPage = () => {
   return (
     <>
       <h1>TODO</h1>
+      <input data-testid="new-todo-input" />
+      <button data-testid="new-todo-add-button">추가</button>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            <span>TODO 1</span>
+          </label>
+          <button data-testid="modify-button">수정</button>
+          <button data-testid="delete-button">삭제</button>
+        </li>
+      </ul>
     </>
   );
 };
