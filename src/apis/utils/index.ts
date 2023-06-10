d00b4e9f7a06acf41745e0b022ef5fc818af1829
Yongjun Park <yopark@student.42seoul.kr>
2023-06-11 02:24:50 +0900

    🐛 fix: token을 instance 생성하자마자 접근하는 오류

--- src/apis/utils/index.ts
+++ src/apis/utils/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 import { ACCESS_TOKEN_KEY, BASE_URL } from '../constants';
 
 const axiosApi = (url: string, headers: { [key: string]: string } = {}) => {
@@ -14,31 +14,24 @@ const axiosJsonApi = (url: string, headers: { [key: string]: string } = {}) => {
   return instance;
 };
 
-const axiosAuthApi = (url: string, headers: { [key: string]: string } = {}) => {
+const accessTokenInjector = (
+  config: InternalAxiosRequestConfig,
+): InternalAxiosRequestConfig => {
   const token = localStorage.getItem(ACCESS_TOKEN_KEY);
 
   if (token === null) {
     throw new Error('no access token in local storage');
   }
-  const instance = axiosApi(url, {
-    ...headers,
-    Authorization: `Bearer ${token}`,
-  });
-  return instance;
-};
-
-const axiosAuthJsonApi = (
-  url: string,
-  headers: { [key: string]: string } = {},
-) => {
-  const instance = axiosAuthApi(url, {
-    ...headers,
-    'Content-Type': 'application/json',
-  });
-  return instance;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 };
 
 export const apiInstance = axiosApi(BASE_URL);
+
 export const apiJsonInstance = axiosJsonApi(BASE_URL);
-export const apiAuthInstance = axiosAuthApi(BASE_URL);
-export const apiAuthJsonInstance = axiosAuthJsonApi(BASE_URL);
+
+export const apiAuthInstance = axiosApi(BASE_URL);
+apiAuthInstance.interceptors.request.use(accessTokenInjector);
+
+export const apiAuthJsonInstance = axiosJsonApi(BASE_URL);
+apiAuthJsonInstance.interceptors.request.use(accessTokenInjector);
