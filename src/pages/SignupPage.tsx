c76ea5b378dfc204daadbb5a2ef3985a47f99a68
Yongjun Park <yopark@student.42seoul.kr>
2023-06-10 18:34:16 +0900

    ✨ feat: markup SignupPage

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -2,6 +2,11 @@ const SignupPage = () => {
   return (
     <>
       <h1>회원가입</h1>
+      <form>
+        <input data-testid="email-input" />
+        <input data-testid="password-input" />
+        <button data-testid="signup-button">회원가입</button>
+      </form>
     </>
   );
 };
