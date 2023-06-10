89335f19c4d90e6f60ea71ace57574dd503b50ae
Yongjun Park <yopark@student.42seoul.kr>
2023-06-11 03:02:52 +0900

    ♻️ refactor: use type not interface in req & res

--- src/apis/api/auth/postSignup.ts
+++ src/apis/api/auth/postSignup.ts
@@ -1,10 +1,12 @@
 import { API_ROUTES } from '@/apis/constants/API_ROUTES';
 import { apiJsonInstance } from '@/apis/utils';
 
-export interface SignupRequest {
+export type SignupRequest = {
   email: string;
   password: string;
-}
+};
+
+export type SignupResponse = void;
 
 export const postSignup = (data: SignupRequest) => {
   return apiJsonInstance.post<void>(API_ROUTES.SIGNUP, data);
