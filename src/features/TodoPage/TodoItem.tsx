7577448a32062df3ff89484599fd942ac66cb181
Yongjun Park <yopark@student.42seoul.kr>
2023-06-11 17:09:43 +0900

    ✨ [Feature] TODO 수정모드 logic (#16)
    * ♻️ refactor: TodoList & TodoItem
    
    * ✨ feat: Todo 수정 모드 뷰
    
    * ✨ feat: 수정 모드 logic
    
    * ♻️ refactor: handleCheckboxClick을 TodoItem 내부에서 처리

---
+++ src/features/TodoPage/TodoItem.tsx
@@ -0,0 +1,136 @@
+import {
+  UpdateTodoRequest,
+  UpdateTodoResponse,
+  putTodo,
+} from '@/apis/api/todos/putTodo';
+import { isAxiosErrorFromWantedPreOnboardingServer } from '@/apis/utils/isAxiosErrorFromWantedPreOnboardingServer';
+import type { Todo } from '@/types/Todo';
+import { useState } from 'react';
+
+type TodoItemProps = {
+  todo: Todo;
+  onDeleteBtnClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  setTodoInTodos: (todo: Todo) => void;
+};
+
+export const TodoItem = ({
+  todo,
+  onDeleteBtnClick,
+  setTodoInTodos,
+}: TodoItemProps) => {
+  const [isModifyMode, setIsModifyMode] = useState<boolean>(false);
+  const [modifyTodoInput, setModifyTodoInput] = useState<string>(todo.todo);
+
+  const handleCheckboxClick = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    e.preventDefault();
+
+    const checked = e.target.checked;
+
+    const { id, todo: todoText } = todo;
+    const newUpdateTodoRequest: UpdateTodoRequest = {
+      todo: todoText,
+      isCompleted: checked,
+    };
+    try {
+      const res = await putTodo(id, newUpdateTodoRequest);
+      if (res.status === 200) {
+        const { id, todo: todoText, isCompleted } = res.data;
+        const newTodo = { id, todo: todoText, isCompleted };
+        setTodoInTodos(newTodo);
+      }
+    } catch (e: unknown) {
+      if (isAxiosErrorFromWantedPreOnboardingServer<UpdateTodoResponse>(e)) {
+        const { message } = e.response.data;
+        alert(message); // TODO: replace with toast
+      }
+    }
+  };
+
+  const handleModifyBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsModifyMode(true);
+  };
+
+  const handleChangeModifyTodoInput = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setModifyTodoInput(value);
+  };
+
+  const handleCancelBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setModifyTodoInput(todo.todo);
+    setIsModifyMode(false);
+  };
+
+  const handleSubmitBtnClick = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    e.preventDefault();
+
+    const { id, isCompleted } = todo;
+    const newUpdateTodoRequest: UpdateTodoRequest = {
+      todo: modifyTodoInput,
+      isCompleted,
+    };
+    if (modifyTodoInput === todo.todo) {
+      return;
+    }
+    try {
+      const res = await putTodo(id, newUpdateTodoRequest);
+      if (res.status === 200) {
+        const { id, todo: todoText, isCompleted } = res.data;
+        const newTodo = { id, todo: todoText, isCompleted };
+        setTodoInTodos(newTodo);
+      }
+    } catch (e: unknown) {
+      if (isAxiosErrorFromWantedPreOnboardingServer<UpdateTodoResponse>(e)) {
+        const { message } = e.response.data;
+        alert(message);
+        return; // 수정 실패 시, 수정 모드 유지
+      }
+    }
+    setIsModifyMode(false);
+  };
+
+  return (
+    <li>
+      {!isModifyMode ? (
+        <>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={handleCheckboxClick}
+            />
+            <span>{todo.todo}</span>
+          </label>
+          <button data-testid="modify-button" onClick={handleModifyBtnClick}>
+            수정
+          </button>
+          <button data-testid="delete-button" onClick={onDeleteBtnClick}>
+            삭제
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            data-testid="modify-input"
+            value={modifyTodoInput}
+            onChange={handleChangeModifyTodoInput}
+          />
+          <button data-testid="submit-button" onClick={handleSubmitBtnClick}>
+            제출
+          </button>
+          <button data-testid="cancel-button" onClick={handleCancelBtnClick}>
+            취소
+          </button>
+        </>
+      )}
+    </li>
+  );
+};
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,16 +1,11 @@
-import { DeleteTodoResponse, deleteTodo } from '@/apis/api/todos/deleteTodo';
 import { GetTodosResponse, getTodos } from '@/apis/api/todos/getTodos';
 import {
   CreateTodoRequest,
   CreateTodoResponse,
   postTodo,
 } from '@/apis/api/todos/postTodo';
-import {
-  UpdateTodoRequest,
-  UpdateTodoResponse,
-  putTodo,
-} from '@/apis/api/todos/putTodo';
 import { isAxiosErrorFromWantedPreOnboardingServer } from '@/apis/utils/isAxiosErrorFromWantedPreOnboardingServer';
+import { TodoList } from '@/features/TodoPage/TodoList';
 import { Todo } from '@/types/Todo';
 import { useEffect, useState } from 'react';
 
@@ -39,61 +34,6 @@ const TodoPage = () => {
       }
     } catch (e: unknown) {
       if (isAxiosErrorFromWantedPreOnboardingServer<CreateTodoResponse>(e)) {
-        const { message } = e.response.data;
-        alert(message); // TODO: replace with toast
-      }
-    }
-  };
-
-  const handleCheckboxClick = async (
-    e: React.ChangeEvent<HTMLInputElement>,
-    todo: Todo,
-  ) => {
-    e.preventDefault();
-
-    const checked = e.target.checked;
-
-    const { id, todo: todoText } = todo;
-    const newUpdateTodoRequest: UpdateTodoRequest = {
-      todo: todoText,
-      isCompleted: checked,
-    };
-    try {
-      const res = await putTodo(id, newUpdateTodoRequest);
-      if (res.status === 200) {
-        const { id, todo: todoText, isCompleted } = res.data;
-        const newTodos = todos.map((todo) => {
-          if (todo.id === id) {
-            const newTodo: Todo = { id, todo: todoText, isCompleted };
-            return newTodo;
-          }
-          return todo;
-        });
-        setTodos(newTodos);
-      }
-    } catch (e: unknown) {
-      if (isAxiosErrorFromWantedPreOnboardingServer<UpdateTodoResponse>(e)) {
-        const { message } = e.response.data;
-        alert(message); // TODO: replace with toast
-      }
-    }
-  };
-
-  const handleDeleteBtnClick = async (
-    e: React.MouseEvent<HTMLButtonElement>,
-    id: number,
-  ) => {
-    e.preventDefault();
-
-    try {
-      const res = await deleteTodo(id);
-      if (res.status === 204) {
-        alert('삭제되었습니다.');
-        const newTodos = todos.filter((todo) => todo.id !== id); // 204 반환되면 서버에서 삭제된 것이므로, 굳이 refetch할 필요 없이 클라이언트에서만 삭제해줘도 됨
-        setTodos(newTodos);
-      }
-    } catch (e: unknown) {
-      if (isAxiosErrorFromWantedPreOnboardingServer<DeleteTodoResponse>(e)) {
         const { message } = e.response.data;
         alert(message); // TODO: replace with toast
       }
@@ -133,27 +73,7 @@ const TodoPage = () => {
       <button data-testid="new-todo-add-button" onClick={handleAddBtnClick}>
         추가
       </button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={(e) => handleCheckboxClick(e, todo)}
-              />
-              <span>{todo.todo}</span>
-            </label>
-            <button data-testid="modify-button">수정</button>
-            <button
-              data-testid="delete-button"
-              onClick={(e) => handleDeleteBtnClick(e, todo.id)}
-            >
-              삭제
-            </button>
-          </li>
-        ))}
-      </ul>
+      <TodoList todos={todos} setTodos={setTodos} />
     </>
   );
 };
